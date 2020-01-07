1cead643a2f3264c575f719849068e412f688e16
Nightzio <nicolaslecoz85@gmail.com>
2020-01-07 14:18:47 +0100

    Ajout du dossier html au 07/01

---
+++ html/getLivesNames.php
@@ -0,0 +1,24 @@
+<?php
+require "constants.php";
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+$dir    = '/usr/local/nginx/tmp/hls/';
+$scanned_dir = array_diff(scandir(URL_HLS), array('..', '.'));
+$streams = "";
+$streams = preg_grep('~\.(m3u8)$~',$scanned_dir);
+$names="";
+
+foreach($streams as $s)
+{
+	$val=substr($s, 0, strrpos($s, "."));
+	 $names = $names.$val.';';
+}
+ 
+	echo $names;
+	
+?>
---
+++ html/getlives.php
@@ -0,0 +1,18 @@
+<?php
+require "constants.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$LivesCount = 0;
+//$dir    = '/usr/local/nginx/tmp/hls/';
+$scanned_dir = array_diff(scandir(URL_HLS), array('..', '.'));
+$streams = "";
+$streams = preg_grep('~\.(m3u8)$~',$scanned_dir);
+			
+$LivesCount = count($streams);
+echo $LivesCount;
+	
+	
+?>
